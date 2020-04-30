761778f551d287c9d5ddd1b014b022ac9f9d5a79
Paulo Antonio <paulo.werder@gmail.com>
2020-04-29 23:23:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cadastro</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        
+        <form action="script.php" method="POST">
+            <h2>Cadastrar Aluno</h2>
+            <p>Diga seu nome: <input type="text" name="nome"></p>
+            <p>Diga sua idade: <input type="text" name="idade"></p>
+            <button type="submit">Cadastrar</button> 
+        </form>
+
+    </body>
+</html>
